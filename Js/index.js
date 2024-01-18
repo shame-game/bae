a5bc13305464faf4cbb417270d65dd8d7b26f65c
shame-game <hoanghung.vam1209@gmail.com>
2024-01-18 11:08:50 +0700

    d

--- Js/index.js
+++ Js/index.js
@@ -4,35 +4,10 @@ const vams = document.querySelectorAll.bind(document);
 /* load */ 
 window.onload = ()=>{
     vam('.loadweb').remove();
+    setTimeout(()=>{
+        vam('.intro').remove()
+    },2500)
 }; 
-
-/* #c661412b2 */ 
-vam('#c661412b2-iframe_click').addEventListener('click', () => {
-    vam('.c661412b2-background_click').setAttribute('style', 'display:flex')
-    vam('.c661412b2-iframe_click').setAttribute('style', 'display:flex')
-    vam('.main').removeAttribute('data-scroll-container')
-    var c661412b2Iframe = `<iframe  class="c661412b2-iframe" src="https://www.youtube.com/embed/2rq6Vph2Sfc?si=xLhapCgHCTZxgnuT" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-    vam('.c661412b2-iframe_click').innerHTML = c661412b2Iframe
-    vam('.c661412b2-background_click').addEventListener('click', () => {
-        vam('.c661412b2-background_click').setAttribute('style', 'display:none')
-        vam('.c661412b2-iframe_click').setAttribute('style', 'display:none')
-        vam('.c661412b2-iframe').remove()
-    })
-})
-/* #c661412b1 */ 
-vam('.c661412b1button-click').addEventListener('click', () => {
-    vam('.c661412b1-background_click').setAttribute('style', 'display:flex')
-    vam('.c661412b1-iframe_click').setAttribute('style', 'display:flex')
-    var c661412b1Iframe = `<iframe  class="c661412b1-iframe" src="https://www.youtube.com/embed/-BTOYVIdgr0?si=H5GXRph-jhJ6i7dM" frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
-    vam('.c661412b1-iframe_click').innerHTML = c661412b1Iframe
-    vam('.c661412b1-background_click').addEventListener('click', () => {
-        vam('.c661412b1-background_click').setAttribute('style', 'display:none')
-        vam('.c661412b1-iframe_click').setAttribute('style', 'display:none')
-        vam('.c661412b1-iframe').remove()
-    })
-})
 
 scroll.on('scroll', (args) => {
     if(typeof args.currentElements['libo1412a1'] === 'object') {
